c1a7b58349fe0cb78a52061976ec26e29f03fe2e
amosquet <artusmosquet@gmail.com>
2026-06-14 20:16:01 -0400

    backend work and support for mobile app

--- test_models.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-from google import genai
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-for model in client.models.list():
-    if 'flash' in model.name.lower():
-        print(model.name)
