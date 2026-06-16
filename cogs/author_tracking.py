45d302abdc50acd1f698cad29a394f40febbf041
amosquet <artusmosquet@gmail.com>
2026-06-15 21:36:41 -0400

    Revert "reee"
    This reverts commit 7cbf66355a76aacd46c0dad0d0408f8ad46efc9a.

--- cogs/author_tracking.py
+++ cogs/author_tracking.py
@@ -30,7 +30,7 @@ class AuthorTracking(commands.Cog):
             raise Exception("PocketBase configuration missing in environment variables.")
         url = self.pb_url if "://" in self.pb_url else f"https://{self.pb_url}"
         pb = PocketBase(url)
-        pb.collection("users").auth_with_password(self.pb_user or "", self.pb_password or "")
+        pb.admins.auth_with_password(self.pb_user, self.pb_password)
         return pb
 
     def cog_unload(self):
