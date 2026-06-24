95d1ae08b68d82afb3c1ec58f4a9d680a935d402
amosquet <artusmosquet@gmail.com>
2026-06-23 23:25:14 -0400

    meh

--- test_requests_none.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-try:
-    requests.post("http://httpbin.org/post", data={"startDate": None}, files={"file": ("test.txt", b"content")})
-    print("Success")
-except Exception as e:
-    print("Error:", e)
